425095f2df6b0de4528ded26fb990f1ef435786f
adam <adam.bjp@gmail.com>
2019-02-05 19:04:17 +0100

    init

---
+++ src/app/components/home/home.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Angulartics2Module } from 'angulartics2';
+
+import { CoreModule } from '../../core';
+import { SharedModule } from '../../shared';
+import { MaterialModule } from '../../shared/material.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { QuoteService } from './quote.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TranslateModule,
+    CoreModule,
+    SharedModule,
+    FlexLayoutModule,
+    MaterialModule,
+    Angulartics2Module,
+    HomeRoutingModule
+  ],
+  declarations: [HomeComponent],
+  providers: [QuoteService]
+})
+export class HomeModule {}
