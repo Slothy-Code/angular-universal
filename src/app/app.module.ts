5af99839d6d74151e9e81a734c50f27f6d585efc
drax98 <drax98@gmail.com>
2019-02-08 21:26:01 +0100

    permissions guard init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {RouterModule} from '@angular/router';
 import {CoreModule} from '@shared/core.module';
-import {AuthenticationGuard} from '@shared/guards/authentication/authentication.guard';
 import {extract} from '@shared/services/i18n/i18n.service';
+import {PermissionGuard} from '@shared/guards/permission/permission.guard';
 
 const routes = [
   { path: '', loadChildren: './pages/landpage/landpage.module#LandpageModule', data: { title: extract('Landpage') }},
   { path: 'login', loadChildren: './pages/login/login.module#LoginModule', data: { title: extract('Login') }},
   { path: 'home', loadChildren: './pages/home/home.module#HomeModule',
-    data: { title: extract('Login') }, canActivate: [AuthenticationGuard]},
+    data: {title: extract('Login')}, canActivate: [PermissionGuard.forPermission('test.test')]
+  },
   { path: 'about', loadChildren: './pages/about/about.module#AboutModule', data: { title: extract('About') }},
 ]
 
