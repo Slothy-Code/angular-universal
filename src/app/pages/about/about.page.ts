17e0490e89b1cd3c75fed7819bbc38e2d5e540ce
adam <adam.bjp@gmail.com>
2019-02-08 15:56:42 +0100

    feat: map feature

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterViewInit, OnDestroy, NgZone} from '@angular/core';
+import * as am4core from '@amcharts/amcharts4/core';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+import * as am4maps from '@amcharts/amcharts4/maps';
+import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldHigh';
+
+am4core.useTheme(am4themes_animated);
 
 @Component({
     selector: 'page-about',
@@ -6,12 +12,42 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./about.page.scss']
 })
 
-export class AboutPage implements OnInit {
+export class AboutPage implements  AfterViewInit, OnDestroy {
+    private chart;
 
-    constructor() {
+    constructor(private zone: NgZone) {}
+
+    ngAfterViewInit() {
+        this.zone.runOutsideAngular(() => {
+
+            this.chart = am4core.createFromConfig({
+                'projection': 'Miller',
+                'geodata': am4geodata_worldLow,
+                'series': [{
+                    'type': 'MapPolygonSeries',
+                    'useGeodata': true,
+                    'include': ['PT', 'ES', 'FR', 'DE'],
+                    'mapPolygons': {
+                        'tooltipText': '{name}',
+                        'states': {
+                            'hover': {
+                                'properties': {
+                                    'fill': '#367B25'
+                                }
+                            }
+                        }
+                    }
+                }]
+            }, 'chartdiv', am4maps.MapChart);
+
+        });
     }
 
-    ngOnInit(): void {
+    ngOnDestroy() {
+        this.zone.runOutsideAngular(() => {
+            if (this.chart) {
+                this.chart.dispose();
+            }
+        });
     }
-
 }
